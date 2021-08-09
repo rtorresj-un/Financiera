5a17cf8f0fb983b5618863b319e3828e81bf7300
rtorresj-un <66235718+rtorresj-un@users.noreply.github.com>
2021-08-08 22:36:48 -0500

    10:36

--- Parcial 2, EF..R
+++ Parcial 2, EF..R
@@ -1,12 +1,15 @@
 # Packages
-library(zoo); library(sandwich); library(gmm)
+library(zoo); library(sandwich); library(gmm); library(data.table)
 # Data set contains daily returns of twenty selected stocks from January 1993 to February 2009, the risk-free rate and the three factors of Fama and French 
 data(Finance)
-head(Finance)
+df<-Finance
+head(df)
+setDT(df, keep.rownames = "Date")[]
 
 #1####
 # Portafolio con 25% de retorno de mercado, 25%WMK, 20%UIS, 20%ORB y 10%MAT.
 
+weights<-c(.25, .25, .2, .2, .1)
 
 
 
